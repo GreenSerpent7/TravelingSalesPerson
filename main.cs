8d605e4d3839badd3c2fa5b89cb49d90be0c8ef3
Kevin Davis <136490293+GreenSerpent7@users.noreply.github.com>
2024-12-26 19:53:18 -0500

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+
+class TravelingSalesperson
+{
+
+ staic void Main ()
+{
+     int[,] graph = {
+        { 0, 10, 15, 20},
+        { 10, 0, 35, 25 },
+        { 15, 35, 0, 30 },
+        { 20, 25, 30, 0 }
+     };
+
+    int n = graph.GetLength(0); 
+    List<int> vertices = Enumerable.Range(1, n - 1).ToList(); 
+}
+
+
+
+
+
+}
